50c8dc073d9848c44f5b89f013134a1ce9599ba9
Tyler Knox <16512508+tyler58546@users.noreply.github.com>
2023-08-06 23:16:21 -0700

    chore: remove unused imports

--- src/main/java/com/tyler58546/speedruntimer/TimerCommand.java
+++ src/main/java/com/tyler58546/speedruntimer/TimerCommand.java
@@ -9,7 +9,6 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class TimerCommand implements CommandExecutor, TabCompleter {
--- src/main/java/com/tyler58546/speedruntimer/TimerPlugin.java
+++ src/main/java/com/tyler58546/speedruntimer/TimerPlugin.java
@@ -2,16 +2,10 @@ package com.tyler58546.speedruntimer;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EnderDragonChangePhaseEvent;
-import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.event.player.PlayerPortalEvent;
-import org.bukkit.event.player.PlayerRespawnEvent;
-import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
